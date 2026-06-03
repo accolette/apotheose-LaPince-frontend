241729fa696b2cfec4bf9db8214a33476c2572ab
Aurore Collet <auroreclt@gmail.com>
2026-06-03 16:30:48 +0200

    feat: add bouton and comportement choices if editing or not

--- src/components/project/tabs/DetailsTab.tsx
+++ src/components/project/tabs/DetailsTab.tsx
@@ -1,18 +1,45 @@
-import { Plus } from "lucide-react";
+import { CircleFadingArrowUpIcon, Plus, Save } from "lucide-react";
+import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { ParticipantsCard } from "../ParticipantsCard";
 import { ProjectDetailsForm } from "../ProjectDetailsForm";
 
 export function DetailsTab() {
+	const [isEditing, setIsEditing] = useState(false);
+
+	function handleClick() {
+		if (isEditing) {
+			// TODO : appeler le PATCH
+			console.log("Sauvegarde");
+		}
+
+		setIsEditing(!isEditing);
+	}
 	return (
 		<div className="flex flex-col gap-6 md:flex-row">
 			<div className="flex-1 space-y-6">
-				<ProjectDetailsForm />
+				<ProjectDetailsForm isEditing={isEditing} />
+				<Button
+					type="button"
+					variant="outline"
+					className="w-full border-dashed"
+					onClick={handleClick}
+				>
+					{isEditing ? (
+						<>
+							<Save className="size-4" />
+							Sauvegarder
+						</>
+					) : (
+						<>
+							<CircleFadingArrowUpIcon className="size-4" />
+							Modifier
+						</>
+					)}
+				</Button>
 			</div>
 			{/* <div className="flex-1">
 				<ParticipantsCard />
-				TODO: créer un contexte pour partager le state qui gère la modale du projet 
-				/Users/w0dr/Oclock/apo/projet-cda-LaPince-frontend/src/components/common/ProjectDialog.tsx
 				<Button
 					type="button"
 					variant="outline"
