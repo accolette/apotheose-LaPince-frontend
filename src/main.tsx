a2a44215d28058e9935553a54efcc600d328b750
LudovicMangone <mangone.ludovic@gmail.com>
2026-06-05 17:03:11 +0200

    feat: add toaster on LoginForm and RegisterForm

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Toaster } from "sonner";
 import "./index.css";
 import App from "./App.tsx";
 import { AuthProvider } from "./context/AuthContext.tsx";
@@ -29,5 +30,6 @@ createRoot(rootElement).render(
 				</CategoriesProvider>
 			</AuthProvider>
 		</QueryClientProvider>
+		<Toaster position="top-right" richColors />
 	</StrictMode>,
 );
