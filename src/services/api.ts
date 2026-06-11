314afc1aff9b333919b78ec123a5691fde5957fe
Aurore Collet <auroreclt@gmail.com>
2026-06-11 17:18:52 +0200

    feat: add catch error and toas in handle delete response service

--- src/services/api.ts
+++ src/services/api.ts
@@ -95,10 +95,33 @@ async function handleResponse<T>(
 
 // Throws 204 if deletion is done
 async function handleDeleteResponse(res: Response): Promise<number> {
+	if (res.status === 401) {
+		toast.error("Votre session a expiré. Veuillez vous reconnecter.");
+		window.dispatchEvent(new Event("auth:unauthorized"));
+	}
+
 	if (!res.ok) {
-		const error = await res.json().catch(() => ({ error: res.statusText }));
+		switch (res.status) {
+			case 429:
+				toast.error(
+					"Vous avez effectué trop de requêtes. Réessayez dans quelques minutes.",
+				);
+				break;
+
+			case 500:
+				toast.error(
+					"Une erreur inattendue est survenue. Veuillez réessayer plus tard.",
+				);
+				break;
+		}
+
+		const error = await res.json().catch(() => ({
+			error: res.statusText,
+		}));
+
 		throw error;
 	}
+
 	if (res.status === 204) {
 		return res.status;
 	}
